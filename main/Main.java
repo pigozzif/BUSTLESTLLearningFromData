ba1f9a554dc38bde1f2304bb148c66c74f88cc4a
federicopigozzi <pigozzife@gmail.com>
2020-03-06 09:40:27 +0100

    Main class

---
+++ main/Main.java
@@ -0,0 +1,17 @@
+import it.units.malelab.jgea.Worker;
+
+import java.io.FileNotFoundException;
+
+
+public class Main extends Worker {
+
+    public static void main(String[] args) throws FileNotFoundException {
+            new Main(args);
+        }
+
+        public Main(String[] args) throws FileNotFoundException {
+            super(args);
+        }
+
+        public void run() { }
+}
