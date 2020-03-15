808edf5e53e7a2cc2704030445cf19d916cfeb7d
federicopigozzi <pigozzife@gmail.com>
2020-03-15 15:52:49 +0100

    new Sign enum

---
+++ main/Expressions/Sign.java
@@ -0,0 +1,29 @@
+package Expressions;
+
+import java.util.function.Function;
+
+
+public enum Sign implements ValueExpression<Function<Double, Double>> {
+
+    PLUS("+", num -> num),
+    MINUS("-", num -> -num);
+
+    private final String string;
+    private final Function<Double, Double> converter;
+
+    Sign(String string, Function<Double, Double> converter) {
+        this.string = string;
+        this.converter = converter;
+    }
+
+    @Override
+    public String toString() {
+        return this.string;
+    }
+
+    @Override
+    public Function<Double, Double> getValue() {
+        return this.converter;
+    }
+
+}
