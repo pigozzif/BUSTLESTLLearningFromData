22cb9a470759c825a0df378ace60a5074ed7a42f
federicopigozzi <pigozzife@gmail.com>
2020-03-15 16:04:06 +0100

    added new signs

--- main/Expressions/CompareSign.java
+++ main/Expressions/CompareSign.java
@@ -6,7 +6,10 @@ import java.util.function.BiFunction;
 public enum CompareSign implements ValueExpression<BiFunction<Double, Double, Double>> {
 
     SMALLER("<", (Double a, Double b) -> b - a),
-    GREATER(">", (Double a, Double b) -> a - b);
+    GREATER(">", (Double a, Double b) -> a - b),
+    SMALLER_OR_EQUAL("<=", (Double a, Double b) -> b - a),
+    GREATER_OR_EQUAL(">=", (Double a, Double b) -> a - b),
+    EQUAL("==", (Double a, Double b) -> - Math.abs(a - b));
 
     private final String sign;
     private final BiFunction<Double, Double, Double> function;
