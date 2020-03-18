56fb55020bb69f6aa41a3852dd8073be8eca0760
federicopigozzi <pigozzife@gmail.com>
2020-03-18 11:45:59 +0100

    back with data

--- main/Expressions/BooleanVariable.java
+++ main/Expressions/BooleanVariable.java
@@ -1,6 +1,5 @@
 package Expressions;
 
-import BuildingBlocks.STLFormulaMapper;
 import BuildingBlocks.TrajectoryRecord;
 import it.units.malelab.jgea.core.Node;
 import eu.quanticol.moonlight.monitoring.temporal.TemporalMonitor;
@@ -23,7 +22,6 @@ public class BooleanVariable implements MonitorExpression {
 
     @Override
     public TemporalMonitor<TrajectoryRecord, Double> createMonitor(List<Node<String>> siblings) {
-        //BooleanConstant firstSibling = (BooleanConstant) STLFormulaMapper.fromStringToValueExpression(siblings.get(0).getContent());
         return TemporalMonitor.atomicMonitor(x -> {if (x.getBool(this.string)) {
                                                 return Double.POSITIVE_INFINITY;} else { return Double.NEGATIVE_INFINITY;}
                                                 });
